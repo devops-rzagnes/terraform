ba5b1e0d82d8c9a251e49faa3867213d28ef3e8a
devops-rzagnes <rzagnes4@gmail.com>
2022-07-08 12:34:07 +0100

    "Committing from IDE @ $(date)"

--- Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/createInstance.tf
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/createInstance.tf
@@ -4,7 +4,7 @@ resource "aws_key_pair" "terraform_key" {
     public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
-resource "aws_instance" "MyFirstInstnace" {
+resource "aws_instance" "MyFirstInstnaceVPC2" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   key_name      = aws_key_pair.terraform_key.key_name
--- Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/security_group.tf
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/security_group.tf
@@ -15,7 +15,14 @@ resource "aws_security_group" "allow-terraform-ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["178.62.11.97/32"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["86.20.234.226/32"]
   }
   
   tags = {
