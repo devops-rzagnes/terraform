9297575f4096ca5316a48b266f8e93a4e63ac713
devops-rzagnes <rzagnes4@gmail.com>
2022-07-08 11:36:45 +0100

    "Committing from IDE @ $(date)"

--- Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/nat.tf
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/nat.tf
@@ -1,38 +1,38 @@
 #Define External IP 
-resource "aws_eip" "levelup-nat" {
+resource "aws_eip" "terraform-nat" {
   vpc = true
 }
 
-resource "aws_nat_gateway" "levelup-nat-gw" {
-  allocation_id = aws_eip.levelup-nat.id
-  subnet_id     = aws_subnet.levelupvpc-public-1.id
-  depends_on    = [aws_internet_gateway.levelup-gw]
+resource "aws_nat_gateway" "terraform-nat-gw" {
+  allocation_id = aws_eip.terraform-nat.id
+  subnet_id     = aws_subnet.terraform-vpc-test-public-1.id
+  depends_on    = [aws_internet_gateway.terraform-vpc2-gw]
 }
 
-resource "aws_route_table" "levelup-private" {
-  vpc_id = aws_vpc.levelupvpc.id
+resource "aws_route_table" "terraform-private" {
+  vpc_id = aws_vpc.terraform-vpc-test.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.levelup-nat-gw.id
+    nat_gateway_id = aws_nat_gateway.terraform-nat-gw.id
   }
 
   tags = {
-    Name = "levelup-private"
+    Name = "terraform-private"
   }
 }
 
 # route associations private
 resource "aws_route_table_association" "level-private-1-a" {
-  subnet_id      = aws_subnet.levelupvpc-private-1.id
-  route_table_id = aws_route_table.levelup-private.id
+  subnet_id      = aws_subnet.terraform-vpc-test-private-1.id
+  route_table_id = aws_route_table.terraform-private.id
 }
 
 resource "aws_route_table_association" "level-private-1-b" {
-  subnet_id      = aws_subnet.levelupvpc-private-2.id
-  route_table_id = aws_route_table.levelup-private.id
+  subnet_id      = aws_subnet.terraform-vpc-test-private-2.id
+  route_table_id = aws_route_table.terraform-private.id
 }
 
 resource "aws_route_table_association" "level-private-1-c" {
-  subnet_id      = aws_subnet.levelupvpc-private-3.id
-  route_table_id = aws_route_table.levelup-private.id
+  subnet_id      = aws_subnet.terraform-vpc-test-private-3.id
+  route_table_id = aws_route_table.terraform-private.id
 }
