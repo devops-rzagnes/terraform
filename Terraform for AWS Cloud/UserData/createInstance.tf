f2392e74c2845d3c2036cacd7f73a0b6233efbd7
devops-rzagnes <rzagnes4@gmail.com>
2022-07-11 16:29:42 +0100

    Committing from IDE @ $(date)

--- Terraform for AWS Cloud/UserData/createInstance.tf
+++ Terraform for AWS Cloud/UserData/createInstance.tf
@@ -40,7 +40,7 @@ resource "aws_ebs_volume" "ebs-volume-1" {
 #Atatch Second EBS volume with AWS Instance
 resource "aws_volume_attachment" "ebs-volume-1-attachment" {
   device_name = "/dev/xvdh"
-  volume_id   = aws_ebs_volume.ebs-volume-1[0].id
+  volume_id   = aws_ebs_volume.ebs-volume-1.id
   instance_id = aws_instance.MyFirstInstnace[0].id
   stop_instance_before_detaching = true
 }
