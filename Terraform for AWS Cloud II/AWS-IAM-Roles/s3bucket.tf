a52f3d819c68b5ba68407fb42281f78235f6c78d
devops-rzagnes <rzagnes4@gmail.com>
2022-07-12 15:17:06 +0100

    Committing from IDE @ $(date)

--- Terraform for AWS Cloud II/AWS-IAM-Roles/s3bucket.tf
+++ Terraform for AWS Cloud II/AWS-IAM-Roles/s3bucket.tf
@@ -1,8 +1,8 @@
 #Create AWS S3 Bucket
 
-resource "aws_s3_bucket" "terraform-s3bucket" {
+resource "aws_s3_bucket_acl" "terraform-s3bucket" {
   bucket = "terraform-bucket-141"
-  acl    = "private"
+  acl   = "private"
 
   tags = {
     Name = "terraform-bucket-141"
