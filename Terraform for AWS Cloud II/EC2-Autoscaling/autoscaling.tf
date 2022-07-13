e8ccf19ba5a0a5cb14a14683db4ccf298d2d6f2c
devops-rzagnes <rzagnes4@gmail.com>
2022-07-13 10:11:44 +0100

    Committing from IDE @ $(date)

--- Terraform for AWS Cloud II/EC2-Autoscaling/autoscaling.tf
+++ Terraform for AWS Cloud II/EC2-Autoscaling/autoscaling.tf
@@ -1,22 +1,24 @@
 #AutoScaling Launch Configuration
-resource "aws_launch_configuration" "levelup-launchconfig" {
-  name_prefix     = "levelup-launchconfig"
+resource "aws_launch_configuration" "terraform-launchconfig" {
+  name_prefix     = "terraform-launchconfig"
   image_id        = lookup(var.AMIS, var.AWS_REGION)
   instance_type   = "t2.micro"
-  key_name        = aws_key_pair.levelup_key.key_name
+  key_name        = aws_key_pair.terraform_key.key_name
+  vpc_security_group_ids = [aws_security_group.allow-terraform-ssh.id]
+
 }
 
 #Generate Key
-resource "aws_key_pair" "levelup_key" {
-    key_name = "levelup_key"
+resource "aws_key_pair" "terraform_key" {
+    key_name = "terraform_key"
     public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
 #Autoscaling Group
-resource "aws_autoscaling_group" "levelup-autoscaling" {
-  name                      = "levelup-autoscaling"
-  vpc_zone_identifier       = ["subnet-9e0ad9f5", "subnet-d7a6afad"]
-  launch_configuration      = aws_launch_configuration.levelup-launchconfig.name
+resource "aws_autoscaling_group" "terraform-autoscaling" {
+  name                      = "terraform-autoscaling"
+  vpc_zone_identifier       = ["eu-west-1a", "eu-west-1b"]
+  launch_configuration      = aws_launch_configuration.terraform-launchconfig.name
   min_size                  = 1
   max_size                  = 2
   health_check_grace_period = 200
@@ -25,24 +27,24 @@ resource "aws_autoscaling_group" "levelup-autoscaling" {
 
   tag {
     key                 = "Name"
-    value               = "LevelUp Custom EC2 instance"
+    value               = "terraform Custom EC2 instance"
     propagate_at_launch = true
   }
 }
 
 #Autoscaling Configuration policy - Scaling Alarm
-resource "aws_autoscaling_policy" "levelup-cpu-policy" {
-  name                   = "levelup-cpu-policy"
-  autoscaling_group_name = aws_autoscaling_group.levelup-autoscaling.name
+resource "aws_autoscaling_policy" "terraform-cpu-policy" {
+  name                   = "terraform-cpu-policy"
+  autoscaling_group_name = aws_autoscaling_group.terraform-autoscaling.name
   adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = "1"
+  scaling_adjustment     = "1"    # Scale nodes one at a time
   cooldown               = "200"
   policy_type            = "SimpleScaling"
 }
 
 #Auto scaling Cloud Watch Monitoring
-resource "aws_cloudwatch_metric_alarm" "levelup-cpu-alarm" {
-  alarm_name          = "levelup-cpu-alarm"
+resource "aws_cloudwatch_metric_alarm" "terraform-cpu-alarm" {
+  alarm_name          = "terraform-cpu-alarm"
   alarm_description   = "Alarm once CPU Uses Increase"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "2"
@@ -53,26 +55,26 @@ resource "aws_cloudwatch_metric_alarm" "levelup-cpu-alarm" {
   threshold           = "30"
 
   dimensions = {
-    "AutoScalingGroupName" = aws_autoscaling_group.levelup-autoscaling.name
+    "AutoScalingGroupName" = aws_autoscaling_group.terraform-autoscaling.name
   }
 
   actions_enabled = true
-  alarm_actions   = [aws_autoscaling_policy.levelup-cpu-policy.arn]
+  alarm_actions   = [aws_autoscaling_policy.terraform-cpu-policy.arn]
 }
 
-#Auto Descaling Policy
-resource "aws_autoscaling_policy" "levelup-cpu-policy-scaledown" {
-  name                   = "levelup-cpu-policy-scaledown"
-  autoscaling_group_name = aws_autoscaling_group.levelup-autoscaling.name
+#Auto Descaling Policy. This is required to minmize cost and ensure the current state of teh machines
+resource "aws_autoscaling_policy" "terraform-cpu-policy-scaledown" {
+  name                   = "terraform-cpu-policy-scaledown"
+  autoscaling_group_name = aws_autoscaling_group.terraform-autoscaling.name
   adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = "-1"
+  scaling_adjustment     = "-1"     # decrease the number of nodes one at a time
   cooldown               = "200"
   policy_type            = "SimpleScaling"
 }
 
 #Auto descaling cloud watch 
-resource "aws_cloudwatch_metric_alarm" "levelup-cpu-alarm-scaledown" {
-  alarm_name          = "levelup-cpu-alarm-scaledown"
+resource "aws_cloudwatch_metric_alarm" "terraform-cpu-alarm-scaledown" {
+  alarm_name          = "terraform-cpu-alarm-scaledown"
   alarm_description   = "Alarm once CPU Uses Decrease"
   comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods  = "2"
@@ -83,9 +85,9 @@ resource "aws_cloudwatch_metric_alarm" "levelup-cpu-alarm-scaledown" {
   threshold           = "10"
 
   dimensions = {
-    "AutoScalingGroupName" = aws_autoscaling_group.levelup-autoscaling.name
+    "AutoScalingGroupName" = aws_autoscaling_group.terraform-autoscaling.name
   }
 
   actions_enabled = true
-  alarm_actions   = [aws_autoscaling_policy.levelup-cpu-policy-scaledown.arn]
+  alarm_actions   = [aws_autoscaling_policy.terraform-cpu-policy-scaledown.arn]
 }
--- Terraform for AWS Cloud II/EC2-Autoscaling/variables.tf
+++ Terraform for AWS Cloud II/EC2-Autoscaling/variables.tf
@@ -1,32 +1,32 @@
 variable "AWS_ACCESS_KEY" {
-    type = string
-    default = "AKIASMSIZOF42P2VUDSZ"
+  type = string
+  default = "AKIAQZ3KABO6QEPZ6U2H"
 }
 
 variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-default = "us-east-2"
+  default = "eu-west-1"
 }
 
 variable "AMIS" {
-    type = map
-    default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
-        us-east-2 = "ami-05692172625678b4e"
-        us-west-2 = "ami-0352d5a37fb4f603f"
-        us-west-1 = "ami-0f40c8f97004632f9"
-    }
+
+  default = {
+    eu-west-1 = "ami-0d71ea30463e0ff8d"
+    eu-west-2 = "ami-030770b178fa9d374"
+    us-west-2 = "ami-078a289ddf4b09ae0"
+    us-west-1 = "ami-0f0f1c02e5e4d9d9f"
+  }
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "levelup_key"
+  default = "terraform_key"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "levelup_key.pub"
+  default = "terraform_key.pub"
 }
 
 variable "INSTANCE_USERNAME" {
-  default = "ubuntu"
+  default = "ec2-user"
 }
