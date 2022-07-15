627ffc639acff60aaa20ec78b2ed177830305bea
devops-rzagnes <rzagnes4@gmail.com>
2022-07-15 11:52:24 +0100

    Committing from IDE @ $(date)

--- Terraform_Advance/conditional_statement/create_instance.tf
+++ Terraform_Advance/conditional_statement/create_instance.tf
@@ -12,11 +12,12 @@ resource "aws_key_pair" "terraform_key" {
 module "ec2_cluster" {
     source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
-    name            = "my-cluster"
+
     ami             = "ami-0d71ea30463e0ff8d"
     instance_type   = "t2.micro"
     subnet_id       = "subnet-088dad867af90fd1e"
     count  = var.environment == "Production" ? 2 : 1       # If production environment, then spin up two instances, else spin up one instance
+    name            = "my-cluster-${count.index}"
     key_name        = aws_key_pair.terraform_key.key_name
     vpc_security_group_ids = [aws_security_group.allow-terraform-ssh.id]   # test
 
