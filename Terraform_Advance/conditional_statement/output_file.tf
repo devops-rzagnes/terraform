bbcbbbd446bfcdf8da75cf49885670450e36f586
devops-rzagnes <rzagnes4@gmail.com>
2022-07-15 11:41:51 +0100

    Committing from IDE @ $(date)

--- Terraform_Advance/conditional_statement/output_file.tf
+++ Terraform_Advance/conditional_statement/output_file.tf
@@ -1,3 +1,5 @@
 output "public_ip" {
-  value = try(module.ec2_cluster[0].public_ip, "")
+  description = "List of public EC2 IPS"
+#  value = try(module.ec2_cluster[0].public_ip, "")
+  value = try(module.ec2_cluster.*.public_ip, "")
 }
