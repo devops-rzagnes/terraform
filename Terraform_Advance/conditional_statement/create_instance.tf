f40ebcecc577e18c123528ade2d245a2aee1e058
devops-rzagnes <rzagnes4@gmail.com>
2022-07-15 15:49:47 +0100

    Committing from IDE @ $(date)

--- Terraform_Advance/conditional_statement/create_instance.tf
+++ Terraform_Advance/conditional_statement/create_instance.tf
@@ -8,18 +8,27 @@ resource "aws_key_pair" "terraform_key" {
     public_key    = file(var.public_key_path)
 }
 
+resource "aws_db_subnet_group" "mariadb-subnets" {
+    name        = "mariadb-subnets"
+    description = "Amazon RDS subnet group"
+    subnet_ids  = [aws_subnet.terraform-vpc-test-private-1.id, aws_subnet.terraform-vpc-test-private-2.id]
+}
+
 
 module "ec2_cluster" {
     source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
 
-    ami             = "ami-0d71ea30463e0ff8d"
+#    ami             = "ami-0d71ea30463e0ff8d"
+    ami           = lookup(var.AMIS, var.AWS_REGION)
     instance_type   = "t2.micro"
     subnet_id       = "subnet-088dad867af90fd1e"
+#    subnet_id = aws_subnet.terraform-vpc-test-public-2.id       ## test
     count  = var.environment == "Production" ? 2 : 1       # If production environment, then spin up two instances, else spin up one instance
     name            = "my-cluster-${count.index}"
     key_name        = aws_key_pair.terraform_key.key_name
     vpc_security_group_ids = [aws_security_group.allow-terraform-ssh.id]   # test
+
 
 
     tags = {
--- Terraform_Advance/conditional_statement/varaible.tf
+++ Terraform_Advance/conditional_statement/varaible.tf
@@ -19,3 +19,13 @@ variable "public_key_path" {
     description = "Public key path"
     default = "~/.ssh/terraform_key.pub"
 }
+
+variable "AMIS" {
+
+    default = {
+        eu-west-1 = "ami-0d71ea30463e0ff8d"
+        eu-west-2 = "ami-030770b178fa9d374"
+        us-west-2 = "ami-078a289ddf4b09ae0"
+        us-west-1 = "ami-0f0f1c02e5e4d9d9f"
+    }
+}
