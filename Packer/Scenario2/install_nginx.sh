83892c0b71980c9be809fb75b4c5223482dc1f18
devops-rzagnes <rzagnes4@gmail.com>
2022-07-17 20:02:51 +0100

    Committing from IDE @ $(date)

--- Packer/Scenario2/install_nginx.sh
+++ Packer/Scenario2/install_nginx.sh
@@ -1,6 +1,5 @@
 sudo yum -y update
-sudo amazon-linux-extras enable epel
-sudo yum -y install epel-release
-sudo yum -y install nginx
-#sudo amazon-linux-extras install -y nginx1
+sudo amazon-linux-extras install -y nginx1
+sudo systemctl enable nginx
+sudo systemctl start nginx
 
