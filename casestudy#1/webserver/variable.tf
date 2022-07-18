d06354be5e95a136e499e5f468cc784661b04e0b
devops-rzagnes <rzagnes4@gmail.com>
2022-07-18 11:45:07 +0100

    Committing from IDE @ 2022-07-18 @ 11:32 GMT

--- casestudy#1/webserver/variable.tf
+++ casestudy#1/webserver/variable.tf
@@ -31,7 +31,7 @@ variable "ENVIRONMENT" {
 
 variable "public_key_path" {
   description = "Public key path"
-  default = "~/.ssh/levelup_key.pub"
+  default = "~/.ssh/terraform_key.pub"
 }
 
 variable "vpc_private_subnet1" {
